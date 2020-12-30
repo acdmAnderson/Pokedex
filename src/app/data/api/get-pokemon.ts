3eb4c99ff376903156d41f4489ff4d2c3fa41f77
anderson.adriano <anderson.adriano@leucotron.com.br>
2020-12-30 00:09:30 -0300

    feat: ensure GetPokemon returns 500 if something error happen

---
+++ src/app/data/api/get-pokemon.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+export class GetPokemon {
+  constructor(private readonly http: HttpClient) {}
+
+  find(): Observable<any> {
+    return new Observable((observer) => {
+      observer.error(
+        new HttpErrorResponse({
+          status: 500,
+          statusText: 'Internal server error',
+        })
+      )
+      observer.complete()
+    })
+  }
+}
