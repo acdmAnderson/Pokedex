156d75db7c4f26bd3feb25d6c3b8c45a43d4cca9
anderson.adriano <anderson.adriano@leucotron.com.br>
2020-12-31 13:24:29 -0300

    test: ensure GetPokemonDetail receive 500 if API returns something error

---
+++ src/app/data/api/get-pokemon-detail.spec.ts
@@ -0,0 +1,53 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GetPokemonDetail } from './get-pokemon-detail';
+
+describe('GetPokemonDetail', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let getPokemonDetail: GetPokemonDetail;
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    getPokemonDetail = new GetPokemonDetail(httpClientSpy as any);
+  });
+
+  it('should receive 500 if API have something error', () => {
+    const sut = getPokemonDetail;
+    const errorResponse = new HttpErrorResponse({
+      status: 500,
+      statusText: 'Internal server error',
+    });
+    const valid_url = 'https://valid_url.com';
+    sut.find(valid_url).subscribe(
+      () => fail('expected an error, not pokemon'),
+      (error: HttpErrorResponse) =>
+        expect(errorResponse.status).toBe(error.status)
+    );
+  });
+
+  //   it('should receive data if API is ok', () => {
+  //     const sut = getPokemon;
+  //     const httpResponse: PokemonDetailModel = {
+  //           id: 1,
+  //           abilities: [
+  //             {
+  //               name: 'valid_ability',
+  //             },
+  //           ],
+  //           height: 10,
+  //           weight: 10,
+  //           name: 'valid_name',
+  //           types: [
+  //             {
+  //               name: 'valid_type',
+  //             },
+  //           ],
+  //     };
+  //     httpClientSpy.get.and.returnValue(of(httpResponse));
+  //     sut.find().subscribe(
+  //       (data: PageModel<PokemonModel>) => {
+  //         expect(data).toEqual(httpResponse);
+  //       },
+  //       (error: HttpErrorResponse) => fail(error)
+  //     );
+  //   });
+});
