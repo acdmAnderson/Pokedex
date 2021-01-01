0ad45756884c33afb76dd11e31db95be82cc3b6e
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-01 19:17:17 -0300

    feat: ensure GetPokemonService receive data if API is ok

--- src/app/data/services/get-pokemon.service.ts
+++ src/app/data/services/get-pokemon.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pagination, Pokemon, PokemonParams } from 'src/app/domain/models';
-import { GetPokemon } from 'src/app/domain/usecases/get-pokemon';
-import { serverError } from '../helpers/http.helper';
+import { GetPokemonUseCase } from 'src/app/domain/usecases/get-pokemon';
+import { GetPokemon } from '../api/get-pokemon';
+import { GetPokemonDetail } from '../api/get-pokemon-detail';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable()
-export class GetPokemonService implements GetPokemon {
+export class GetPokemonService implements GetPokemonUseCase {
+  constructor(
+    private readonly getPokemon: GetPokemon,
+    private readonly getPokemonDetail: GetPokemonDetail
+  ) {}
   find(params: PokemonParams): Observable<Pagination<Pokemon>> {
-    return new Observable((observer) => {
-      observer.error(serverError());
-      observer.complete();
-    });
+    return this.getPokemon.find(params).pipe(
+      mergeMap(async (page) => {
+        const { count, results } = page;
+        const pokemonDetail = [];
+        for (const { url } of results) {
+          const pokemonDetailt = await this.getPokemonDetail
+            .find(url)
+            .toPromise();
+          pokemonDetail.push({
+            ...pokemonDetailt,
+            height: `${pokemonDetailt.height} dm`,
+            weight: `${pokemonDetailt.weight} hg`,
+          });
+        }
+        return {
+          count,
+          next: 0,
+          previous: 1,
+          results: pokemonDetail,
+        };
+      })
+    );
   }
 }
