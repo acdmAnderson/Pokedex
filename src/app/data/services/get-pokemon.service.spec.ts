e03617993c76fd326d639a415e92158068d2c26e
anderson.adriano <anderson.adriano@leucotron.com.br>
2020-12-31 21:09:04 -0300

    test: ensure GetPokemonService receive 500 if API have something wrong

---
+++ src/app/data/services/get-pokemon.service.spec.ts
@@ -0,0 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
+import { PokemonParams } from '../models';
+import { GetPokemonService } from './get-pokemon.service';
+
+describe('GetPokemonService', () => {
+  const params: PokemonParams = {
+    limit: 1,
+    offset: 0,
+  };
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [GetPokemonService],
+    }).compileComponents();
+  }));
+
+  it('should receive 500 if API have some error ', () => {
+    const sut = TestBed.inject(GetPokemonService);
+    sut.find().subscribe(
+      () => fail('Expected a error not a pokemon'),
+      (error: HttpErrorResponse) => expect(error.status).toBe(500)
+    );
+  });
+});
