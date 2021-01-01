410102100004d26896d3328426021de76e8d67bd
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-01 17:55:08 -0300

    test: ensure GetPokemonService receive 404 if API returns not found

--- src/app/data/services/get-pokemon.service.spec.ts
+++ src/app/data/services/get-pokemon.service.spec.ts
@@ -1,5 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { async, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { notFound } from '../helpers/http.helper';
 import { PokemonParams } from '../models';
 import { GetPokemonService } from './get-pokemon.service';
 
@@ -14,11 +16,25 @@ describe('GetPokemonService', () => {
     }).compileComponents();
   }));
 
-  it('should receive 500 if API have some error ', () => {
+  it('should receive 500 if API have something error ', () => {
     const sut = TestBed.inject(GetPokemonService);
     sut.find(params).subscribe(
       () => fail('Expected a error not a pokemon'),
       (error: HttpErrorResponse) => expect(error.status).toBe(500)
+    );
+  });
+
+  it('should receive 404 if API returns not found', () => {
+    const sut = TestBed.inject(GetPokemonService);
+    spyOn(sut, 'find').and.returnValues(
+      new Observable((observer) => {
+        observer.error(notFound());
+        observer.complete();
+      })
+    );
+    sut.find(params).subscribe(
+      () => fail('Expected a error not a pokemon'),
+      (error: HttpErrorResponse) => expect(error.status).toBe(404)
     );
   });
 });
