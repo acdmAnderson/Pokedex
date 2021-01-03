1cd4689a91ff81f69e4725c78cf95071a0d490bf
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-03 16:49:45 -0300

    feat: ensure GetPokemonByName receive 500 if something unexpected happens

--- src/app/data/services/get-pokemon-by-name.service.ts
+++ src/app/data/services/get-pokemon-by-name.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pagination, Pokemon } from 'src/app/domain/models';
 import { GetPokemonByNameUseCase } from 'src/app/domain/usecases/get-pokemon-by-name';
+import { GetPokemonDetail } from '../api/get-pokemon-detail';
+import { serverError } from '../helpers/http.helper';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetPokemonByNameService extends GetPokemonByNameUseCase {
+  constructor(private readonly getPokemonDetail: GetPokemonDetail) {
+    super();
+  }
+
   findByName(pokemonName: string): Observable<Pagination<Pokemon>> {
-    return null;
+    return new Observable((observer) => {
+      observer.error(serverError());
+      observer.complete();
+    });
   }
 }
