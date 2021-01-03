97181109d1d4d70e4a0079b5b5aa452424365e33
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-03 14:42:06 -0300

    feat: ensure TopNavbarComponent be created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import {
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { TopNavbarComponent } from './presentation/components/top-navbar/top-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, PokedexComponent],
+  declarations: [AppComponent, PokedexComponent, TopNavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
