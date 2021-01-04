247bef75e88494fc6b4cd31522380bd510bdf3d5
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-04 10:58:21 -0300

    fix: add dependencies to eliminate warnings

--- src/app/presentation/main.component.spec.ts
+++ src/app/presentation/main.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
 
 import { MainComponent } from './main.component';
 
@@ -8,8 +12,10 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MainComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      declarations: [MainComponent, PokedexComponent],
     }).compileComponents();
+    TestBed.inject(HttpClient);
   }));
 
   beforeEach(() => {
