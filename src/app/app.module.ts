b2f4e66fe9d58941d1eefcd0cb815f00f9d91431
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-04 13:02:02 -0300

    refactor: move font awesome imports to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import {
-  FontAwesomeModule,
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import { MainModule } from './presentation/main.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    MainModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(library: FaIconLibrary) {
-    library.addIconPacks(fab, far, fas);
-  }
-}
+export class AppModule {}
--- src/app/presentation/main.module.ts
+++ src/app/presentation/main.module.ts
@@ -4,10 +4,27 @@ import { MainComponent } from './main.component';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [MainComponent, PokedexComponent],
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FontAwesomeModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MainModule {}
+export class MainModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fab, far, fas);
+  }
+}
